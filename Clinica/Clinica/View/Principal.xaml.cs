c185f07e779cc945ed1d152f8761a0897c9cc3db
Jonathan Arango <jonathan.arango@utp.edu.co>
2023-05-03 19:03:00 -0500

    Agregar archivos de proyecto.

---
+++ Clinica/Clinica/View/Principal.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Clinica.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Clinica
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Principal : ContentPage
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        async void Btn_login_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
+    }
+}
---
+++ Clinica/Clinica/ViewModel/RegistroViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+
+
+
+namespace Clinica.ViewModel
+{
+    internal class RegistroViewModel : BaseViewModel
+    {
+
+
+
+        #region
+
+      
+        #endregion
+
+
+        #region Method
+
+       
+
+#endregion
+
+    }
+}
